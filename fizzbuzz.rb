d03f64d792bd5adbe8b76c753357d02414131483
srappaport <sarah.rappaport@greendot.org>
2018-05-04 20:09:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,10 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 3 == 0 && int !% 5 == 0
     "Fizz"
-  else int % 5 ==0
+  elsif int % 5 ==0 && int !% 3 ==0
     "Buzz"
+  elsif int % 5 == 0 && int % 3==0
+    "FizzBuzz"
+  else
   end
 end
